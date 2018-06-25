4756ff0e1c17d74bdfaba55f8e53bae301a9171f
Yarden Chen <yardench@gmail.com>
2018-06-25 12:58:08 +0300

    Added the begging of the classification of words

---
+++ nlp.py
@@ -0,0 +1,26 @@
+
+from textblob import TextBlob
+ 
+
+def get_tweet_sentiment(tweet):
+    '''
+    Utility function to classify sentiment of passed tweet
+    using textblob's sentiment method
+    '''
+    # create TextBlob object of passed tweet text
+    analysis = TextBlob(tweet)
+    # set sentiment
+    if analysis.sentiment.polarity > 0:
+        return 'positive'
+    elif analysis.sentiment.polarity == 0:
+        return 'neutral'
+    else:
+        return 'negative'
+ 
+ 
+def main():
+    print(get_tweet_sentiment("rude"))
+
+if __name__ == "__main__":
+# calling main function
+    main()
